3de0a2ba374d184889c38ffa354ddef8d3ab682b
Mateo Catalogna <catalognamateo@gmail.com>
2025-12-13 17:50:24 -0300

    arreglo deploy

--- snaplink/src/main.ts
+++ snaplink/src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet'; // 1. Importar Helmet
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.use(helmet());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, 
+      forbidNonWhitelisted: true, 
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: ['https://snap-link-frontend-nine.vercel.app/'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
